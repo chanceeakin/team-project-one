6d6d28c5c46aa3f5da5006fd6452344ca6d49f5d
chanceeakin <chance.eakin@gmail.com>
2016-06-09 19:00:06 -0500

    google API work

--- assets/javascript/eakinajaxmaptest.js
+++ assets/javascript/eakinajaxmaptest.js
@@ -1,15 +1,58 @@
-$(document).ready(function() {
-    $.ajax({
-            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Austin,+TX&key=AIzaSyDaaBh-p6JLeuyUa265yJu6qdXcw4nt56g',
-            type: 'GET',
-        })
-        .done(function(response) {
-            console.log(response);
-        })
-        .fail(function(response) {
-            console.log("error");
-        })
-        .always(function(response) {
-            console.log("complete");
-        });
-});
+var map;
+var infowindow;
+var MyLocation = {
+    LAT: 30.29128,
+    LNG: -97.73858
+}
+var newSearch = 'food';
+
+
+
+function initMap() {
+    var active = { lat: MyLocation.LAT, lng: MyLocation.LNG };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: active,
+        zoom: 12
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: active,
+        radius: 5000,
+        type: [newSearch]
+    }, callback);
+    console.log(newSearch);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
+
+$('#submit').on('click', function() {
+    newSearch = $('#address').val().trim();
+    newSearch = "'" + newSearch + "'";
+        initMap();
+})
+
+////////////////////////////////////////////////////////////////
+//create a places search box. then weep.
+///////////////////////////////////////////////////////////////
---
+++ assets/javascript/googlemapsbrainstorm.md
@@ -0,0 +1,7 @@
+The Google Maps API is hella complex.
+
+In lieu of figuring out THE ENTIRE THING: what do we need the map to do?
+
+Print a static map.
+drop 3 markers.
+populate markers based on three variables.
